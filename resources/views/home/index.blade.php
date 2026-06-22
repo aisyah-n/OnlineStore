6516159562db0593469574440f2487f8e7f11a3d
aisyah-n <aisyahh.aais@gmail.com>
2026-06-22 11:59:27 +0800

    fix home images

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -19,17 +19,17 @@
 <div class="row">
 
     <div class="col-md-4 col-lg-4 mb-2">
-        <img src="{{ asset('/img/game.png') }}"
+        <img src="{{ asset('/storage/game.png') }}"
              class="img-fluid rounded">
     </div>
 
     <div class="col-md-4 col-lg-4 mb-2">
-        <img src="{{ asset('/img/safe.png') }}"
+        <img src="{{ asset('/storage/safe.png') }}"
              class="img-fluid rounded">
     </div>
 
     <div class="col-md-4 col-lg-4 mb-2">
-        <img src="{{ asset('/img/submarine.png') }}"
+        <img src="{{ asset('/storage/submarine.png') }}"
              class="img-fluid rounded">
     </div>
 
